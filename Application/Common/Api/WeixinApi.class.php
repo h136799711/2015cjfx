dfc272bfc0fc622218c0a49a1cbf5c2e7e60a0eb
hebidu <hebiduhebi@126.com>
2015-03-18 11:39:44 +0800

    增加getBaseUserInfo方法静默获取用户消息

--- Application/Common/Api/WeixinApi.class.php
+++ Application/Common/Api/WeixinApi.class.php
@@ -238,6 +238,35 @@ class WeixinApi {
 		}
 	}
 	
+	/**
+	 * 静默获取用户信息
+	 * @param $openid 用户的openid
+	 * subscribe	用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+	 * openid	用户的标识，对当前公众号唯一
+	 * nickname	用户的昵称
+	 * sex	用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	 * city	用户所在城市
+	 * country	用户所在国家
+	 * province	用户所在省份
+	 * language	用户的语言，简体中文为zh_CN
+	 * headimgurl	用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+	 * subscribe_time	用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+	 * unionid	只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。详见：获取用户个人信息（UnionID机制）
+	 */
+	public function getBaseUserInfo($openid){
+		/**
+		 * 
+		 */	
+		$accessToken = $this->getAccessToken();
+		$url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$accessToken."&openid=".$openid."&lang=zh_CN";
+		$arr = json_decode($this->curlGet($url),true);
+		if(isset($arr['errcode'])){
+			return array('status'=>false,'msg'=> $this->errcode[$arr['errcode']] );
+		}else{
+			return array('status'=>true,'msg'=>$arr);
+		}
+	}
+	
 	//===============================================================
 
 	private function curlPost($url, $data) {
