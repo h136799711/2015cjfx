03a22285519e1edcc11ea024107a9fb0fc7a10aa
hebidu <hebiduhebi@126.com>
2015-03-19 14:59:48 +0800

    去除Home模块 ，此模块留作后用

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,5 +1,11 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-	'SESSION_PREFIX'=>'Home_'
+	//'配置项'=>'配置值',
+	'DEFAULT_THEME'=>'default',
+	'SESSION_PREFIX'=>'Home_',
+    // 数据库配置
+    'DB_TYPE'                   =>  'mysql',
+    'DB_NAME'                   =>  'boye_2015cjfx',
+    'DB_PORT'                   =>  '3306',
+    'DB_PREFIX'                 =>  'cjfx_',
 );
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,23 +1,86 @@
 <?php
 namespace Home\Controller;
-use Think\Controller;
-class IndexController extends Controller {
+
+class IndexController extends HomeController {
 
 	private $wxaccount = "";
 	private $wxapi = "";
-	
-	protected function _initialize(){
-		$this->refreshWxaccount();
+	private $openid = "";
+
+	protected function _initialize() {
+		parent::_initialize();
+		$this -> refreshWxaccount();
 	}
 
 	public function index() {
-		if (IS_GET) {
-			$userinfo = $this->wxapi ->getBaseUserInfo();
-			$this -> assign("userinfo", $userinfo);
-			$this -> display();
-		} else {
-			$this -> error("无法访问！");
+		$this->redirect(U('Shop/Index/index'));
+//		if (IS_GET) {
+//			session("userinfo", null);
+//			$userinfo = null;
+//			if (session("?userinfo")) {
+//				$userinfo = session("userinfo");
+//			}
+//			if (!is_array($userinfo)) {
+//
+//				$code = I('get.code', '');
+//				$state = I('get.state', '');
+//				if (empty($code) && empty($state)) {
+//
+//					$redirect = $this -> wxapi -> getOAuth2BaseURL(C('SITE_URL') . U('Home/Index/index', array('token' => I('get.token', ''))), 'HomeIndexOpenid');
+//
+//					redirect($redirect);
+//				}
+//
+//				if ($state == 'HomeIndexOpenid') {
+//					$accessToken = $this -> wxapi -> getOAuth2AccessToken($code);
+//
+//					$this -> assign("accessToken", $accessToken);
+//					$this->openid = $accessToken['openid'];
+//					$userinfo = $this -> wxapi -> getBaseUserInfo($accessToken['openid']);
+//					
+//					if ($userinfo['status']) {
+//						$userinfo = $userinfo['info'];
+//						$this->refreshWxuser($userinfo);
+//						session("userinfo", $userinfo);
+//					} else {
+//						$userinfo = null;
+//					}
+//				}
+//			}
+//
+//			$this -> assign("userinfo", $userinfo);
+//			$this -> display();
+//		} else {
+//			$this -> error("无法访问！");
+//		}
+	}
+
+	/**
+	 * 刷新粉丝信息
+	 */
+	private function refreshWxuser($userinfo) {
+		$wxuser = array();
+		$wxuser['wxaccount_id'] = intval($this -> wxaccount['id']);
+		$wxuser['nickname'] = $userinfo['nickname'];
+		$wxuser['province'] = $userinfo['province'];
+		$wxuser['country'] = $userinfo['country'];
+		$wxuser['city'] = $userinfo['city'];
+		$wxuser['sex'] = $userinfo['sex'];
+		$wxuser['avatar'] = $userinfo['headimgurl'];
+		$wxuser['subscribe_time'] = $userinfo['subscribe_time'];
+		
+		if(!empty($this->openid) && is_array($this->wxaccount)){
+		
+			$map = array('openid' => $this -> openid, 'wxaccount_id' => $this->wxaccount['id'] );
+			
+			$result = apiCall('Weixin/Wxuser/save', array($map, $wxuser));
+			
+			if(!$result['status']){
+				LogRecord($result['info'], "[Home/Index/refreshWxuser]".__LINE__);
+			}
+		
 		}
+				
 	}
 
 	/**
@@ -25,17 +88,17 @@ class IndexController extends Controller {
 	 */
 	private function refreshWxaccount() {
 		$token = I('get.token', '');
-		if(!empty($token)){
-			session("token",$token);
-		}elseif(session("?token")){
+		if (!empty($token)) {
+			session("token", $token);
+		} elseif (session("?token")) {
 			$token = session("token");
 		}
-		
+
 		$result = apiCall('Weixin/Wxaccount/getInfo', array( array('token' => $token)));
 		if ($result['status']) {
 			$this -> wxaccount = $result['info'];
-     		$this->wxapi = new \Common\Api\WeixinApi($this->wxaccount['appid'],$this->wxaccount['appsecret']);		
-		}else{
+			$this -> wxapi = new \Common\Api\WeixinApi($this -> wxaccount['appid'], $this -> wxaccount['appsecret']);
+		} else {
 			$this -> error("信息获取失败，请重试！");
 		}
 	}
