3b9fbb46528679b2f2426bbdb05678f86e78a4c5
hebidu <hebiduhebi@126.com>
2015-03-20 15:23:57 +0800

    将有2个模块以上使用的Model类移动到Common模块中

---
+++ Application/Common/Model/CommissionModel.class.php
@@ -0,0 +1,18 @@
+<?php
+// .-----------------------------------------------------------------------------------
+// | WE TRY THE BEST WAY
+// |-----------------------------------------------------------------------------------
+// | Author: 贝贝 <hebiduhebi@163.com>
+// | Copyright (c) 2013-2016 杭州博也网络科技, http://www.itboye.com. All Rights Reserved.
+// |-----------------------------------------------------------------------------------
+
+namespace Common\Model;
+use Think\Model;
+
+class CommissionModel  extends  Model{
+	
+	protected $_auto = array(
+		array('updatetime',NOW_TIME,self::MODEL_BOTH)
+	);
+		
+}
